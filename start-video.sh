1c248755681d9bb96f39f4449e8ce5ab23f05979
Renning Bruns <ren@renmail.net>
2020-11-26 16:18:59 -0800

    some notes and 15fps

--- start-video.sh
+++ start-video.sh
@@ -2,16 +2,22 @@
 
 rm -f stream.m3u8 stream*.ts
 
+# input_format: ffmpeg -f v4l2 -list_formats all -i /dev/video0
+# framerate and video_size: v4l2-ctl --list-formats-ext
+
 exec ffmpeg \
-	-f v4l2 -framerate 20 -video_size 640x360 -thread_queue_size 1024 -i /dev/video0 \
-	-f alsa -ac 1 -thread_queue_size 1024 -i hw:1 \
+	-f v4l2 \
+		-input_format yuyv422 \
+		-framerate 15 -video_size 640x480 \
+		-i /dev/video0 \
+	-f alsa -ac 1 -i hw:1 \
 	-c:v libx264 \
 		-crf 21 \
 		-profile:v main -level 3.1 \
 		-pix_fmt yuv420p \
 		-preset veryfast \
 		-tune zerolatency \
-		-g 20 \
+		-g 15 \
 		-sc_threshold 0 \
 	-c:a aac -b:a 40k -ac 2 \
 	-f hls -hls_time 6 -hls_list_size 6 -hls_flags delete_segments+program_date_time stream.m3u8
