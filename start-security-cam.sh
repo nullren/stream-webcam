76892032f678a0e7c165e408c0d033b9aaf6651c
Renning Bruns <ren@renmail.net>
2020-11-27 16:32:07 -0800

    make sure we know to pass in rtsp url, annoying if not

--- start-security-cam.sh
+++ start-security-cam.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -f stream.m3u8 stream*.ts
 
 # should pass in the rtsp url that is already h264 yuv420p and aac
 input=$1
+if [ "${input:0:7}" != "rtsp://" ]; then
+	echo usage: $0 rtsp://somestream
+	exit 1
+fi
 
 exec ffmpeg \
 	-i "$input" \
